9cdecbccc8ab40968bcb3b2051fc1b05bb168f01
wam <dorothy_007@naver.com>
2023-11-10 18:59:29 +0900

    module_basic #2 모듈 방식

--- module_basic/hello.js
+++ module_basic/hello.js
@@ -1,3 +1,13 @@
-function hello() {
-  console.log("HELLO");
+/* 방법 1, 방법 2 */
+export function hello1() {
+  console.log("HELLO 1");
+}
+
+export function hello2() {
+  console.log("HELLO 2");
+}
+
+/* 방법 3 */
+export default function hello3() {
+  console.log("HELLO 3");
 }
--- module_basic/main.js
+++ module_basic/main.js
@@ -1,1 +1,16 @@
-hello();
+/* 방법 1 */
+import { hello1, hello2 } from "./hello.js";
+
+hello1();
+hello2();
+
+/* 방법 2 */
+import * as hello from "./hello.js";
+
+hello.hello1();
+hello.hello2();
+
+/* 방법 3 */
+import hello3 from "./hello.js";
+
+hello3();

17a42fa383df0c7960052c9195351108ed949917
wam <dorothy_007@naver.com>
2024-11-10 01:07:56 +0900

    SpotLight
    프로젝트명 10_light (조명)

--- 10_light/light/src/main.js
+++ 10_light/light/src/main.js
@@ -1,5 +1,6 @@
 // import example from "./ex01_light_basic";
 // import example from "./ex02_light_shadow";
-import example from "./ex03_pointLight";
+// import example from "./ex03_pointLight";
+import example from "./ex04_spotLight";
 
 example();

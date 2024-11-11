fd140b8879c627422758e8cefcf896ea7a3feeec
wam <dorothy_007@naver.com>
2024-11-11 18:41:28 +0900

    HemisphereLight
    프로젝트명 10_light (조명)

--- 10_light/light/src/main.js
+++ 10_light/light/src/main.js
@@ -1,6 +1,7 @@
 // import example from "./ex01_light_basic";
 // import example from "./ex02_light_shadow";
 // import example from "./ex03_pointLight";
-import example from "./ex04_spotLight";
+// import example from "./ex04_spotLight";
+import example from "./ex05_hemisphereLight";
 
 example();

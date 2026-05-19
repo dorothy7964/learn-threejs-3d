375fffd111c39bdc5c1dd1ba9dad1a7a3821defe
wam <dorothy_007@naver.com>
2026-05-19 21:03:38 +0900

    여러개의 Scene 사용하기_CSS 자동 반영(HMR) 설정, 레이아웃 적용
    프로젝트명 17_multiCanvas

--- 17_multiCanvas/multicanvas_webpack/src/main.js
+++ 17_multiCanvas/multicanvas_webpack/src/main.js
@@ -1,34 +1,45 @@
-import * as THREE from 'three';
+import * as THREE from "three";
+import "./main.css";
 
-// ----- 주제: 여러개의 캔버스 사용하기
+/* 주제: 주제: 여러개의 캔버스 사용하기 */
 
-// Renderer
-const canvas = document.querySelector('#three-canvas');
+/* ===============================
+  ======= Renderer =======
+=============================== */
+const canvas = document.querySelector("#three-canvas");
 const renderer = new THREE.WebGLRenderer({
-	canvas,
-	antialias: true
+  canvas,
+  antialias: true
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
 
-// 그리기
+/* ===============================
+	======= Scene 만들기 =======
+=============================== */
+
+/* ===============================
+  ======= 그리기 =======
+=============================== */
 const clock = new THREE.Clock();
 
 function draw() {
-	const delta = clock.getDelta();
+  const delta = clock.getDelta();
 
-	renderer.render(scene, camera);
-	window.requestAnimationFrame(draw);
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(draw);
 }
 
 function setSize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.render(scene, camera);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
 }
 
-// 이벤트
-window.addEventListener('resize', setSize);
+/* ===============================
+	======= 이벤트 =======
+=============================== */
+window.addEventListener("resize", setSize);
 
 draw();

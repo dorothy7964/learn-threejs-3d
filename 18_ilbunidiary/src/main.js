1bd7341accdd3729295656af70bb9142e5f277d5
wam <dorothy_007@naver.com>
2026-05-30 00:12:10 +0900

    주석 추가
    프로젝트명 18_ilbunidiary

--- 18_ilbunidiary/src/main.js
+++ 18_ilbunidiary/src/main.js
@@ -1,39 +1,39 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { Player } from './Player';
-import { House } from './House';
-import gsap from 'gsap';
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Player } from "./Player";
+import { House } from "./House";
+import gsap from "gsap";
 
 // Texture
 const textureLoader = new THREE.TextureLoader();
-const floorTexture = textureLoader.load('/images/grid.png');
+const floorTexture = textureLoader.load("/images/grid.png");
 floorTexture.wrapS = THREE.RepeatWrapping;
 floorTexture.wrapT = THREE.RepeatWrapping;
 floorTexture.repeat.x = 10;
 floorTexture.repeat.y = 10;
 
 // Renderer
-const canvas = document.querySelector('#three-canvas');
+const canvas = document.querySelector("#three-canvas");
 const renderer = new THREE.WebGLRenderer({
-	canvas,
-	antialias: true
+  canvas,
+  antialias: true
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
 renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; // 그림자 가장자리를 부드럽게 처리
 
 // Scene
 const scene = new THREE.Scene();
 
 // Camera
 const camera = new THREE.OrthographicCamera(
-	-(window.innerWidth / window.innerHeight), // left
-	window.innerWidth / window.innerHeight, // right,
-	1, // top
-	-1, // bottom
-	-1000,
-	1000
+  -(window.innerWidth / window.innerHeight), // left
+  window.innerWidth / window.innerHeight, // right,
+  1, // top
+  -1, // bottom
+  -1000,
+  1000
 );
 
 const cameraPosition = new THREE.Vector3(1, 5, 5);
@@ -43,10 +43,10 @@ camera.updateProjectionMatrix();
 scene.add(camera);
 
 // Light
-const ambientLight = new THREE.AmbientLight('white', 0.7);
+const ambientLight = new THREE.AmbientLight("white", 0.7);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight('white', 3);
+const directionalLight = new THREE.DirectionalLight("white", 3);
 const directionalLightOriginPosition = new THREE.Vector3(1, 1, 1);
 directionalLight.position.x = directionalLightOriginPosition.x;
 directionalLight.position.y = directionalLightOriginPosition.y;
@@ -54,8 +54,10 @@ directionalLight.position.z = directionalLightOriginPosition.z;
 directionalLight.castShadow = true;
 
 // mapSize 세팅으로 그림자 퀄리티 설정
+// (주의) 값이 클수록 그림자는 선명해지지만 성능 부담이 커짐
 directionalLight.shadow.mapSize.width = 2048;
 directionalLight.shadow.mapSize.height = 2048;
+
 // 그림자 범위
 directionalLight.shadow.camera.left = -100;
 directionalLight.shadow.camera.right = 100;
@@ -68,59 +70,59 @@ scene.add(directionalLight);
 // Mesh
 const meshes = [];
 const floorMesh = new THREE.Mesh(
-	new THREE.PlaneGeometry(100, 100),
-	new THREE.MeshStandardMaterial({
-		map: floorTexture
-	})
+  new THREE.PlaneGeometry(100, 100),
+  new THREE.MeshStandardMaterial({
+    map: floorTexture
+  })
 );
-floorMesh.name = 'floor';
-floorMesh.rotation.x = -Math.PI/2;
+floorMesh.name = "floor";
+floorMesh.rotation.x = -Math.PI / 2;
 floorMesh.receiveShadow = true;
 scene.add(floorMesh);
 meshes.push(floorMesh);
 
 const pointerMesh = new THREE.Mesh(
-	new THREE.PlaneGeometry(1, 1),
-	new THREE.MeshBasicMaterial({
-		color: 'crimson',
-		transparent: true,
-		opacity: 0.5
-	})
+  new THREE.PlaneGeometry(1, 1),
+  new THREE.MeshBasicMaterial({
+    color: "crimson",
+    transparent: true,
+    opacity: 0.5
+  })
 );
-pointerMesh.rotation.x = -Math.PI/2;
+pointerMesh.rotation.x = -Math.PI / 2;
 pointerMesh.position.y = 0.01;
 pointerMesh.receiveShadow = true;
 scene.add(pointerMesh);
 
 const spotMesh = new THREE.Mesh(
-	new THREE.PlaneGeometry(3, 3),
-	new THREE.MeshStandardMaterial({
-		color: 'yellow',
-		transparent: true,
-		opacity: 0.5
-	})
+  new THREE.PlaneGeometry(3, 3),
+  new THREE.MeshStandardMaterial({
+    color: "yellow",
+    transparent: true,
+    opacity: 0.5
+  })
 );
 spotMesh.position.set(5, 0.005, 5);
-spotMesh.rotation.x = -Math.PI/2;
+spotMesh.rotation.x = -Math.PI / 2;
 spotMesh.receiveShadow = true;
 scene.add(spotMesh);
 
 const gltfLoader = new GLTFLoader();
 
 const house = new House({
-	gltfLoader,
-	scene,
-	modelSrc: '/models/house.glb',
-	x: 5,
-	y: -1.3,
-	z: 2
+  gltfLoader,
+  scene,
+  modelSrc: "/models/house.glb",
+  x: 5,
+  y: -1.3,
+  z: 2
 });
 
 const player = new Player({
-	scene,
-	meshes,
-	gltfLoader,
-	modelSrc: '/models/ilbuni.glb'
+  scene,
+  meshes,
+  gltfLoader,
+  modelSrc: "/models/ilbuni.glb"
 });
 
 const raycaster = new THREE.Raycaster();
@@ -133,171 +135,160 @@ let isPressed = false; // 마우스를 누르고 있는 상태
 const clock = new THREE.Clock();
 
 function draw() {
-	const delta = clock.getDelta();
+  const delta = clock.getDelta();
 
-	if (player.mixer) player.mixer.update(delta);
+  if (player.mixer) player.mixer.update(delta);
 
-	if (player.modelMesh) {
-		camera.lookAt(player.modelMesh.position);
-	}
+  // 모델 로딩 전 오류 방지 (lookAt 안전 처리) : 모델 로딩 전에 실행되는 오류를 막기 위한 체크
+  if (player.modelMesh) {
+    camera.lookAt(player.modelMesh.position);
+  }
 
-	if (player.modelMesh) {
+  if (player.modelMesh) {
+    if (isPressed) {
+      raycasting();
+    }
 
-		if (isPressed) {
-			raycasting();
-		}
+    if (player.moving) {
+      // 걸어가는 상태
+      angle = Math.atan2(
+        destinationPoint.z - player.modelMesh.position.z,
+        destinationPoint.x - player.modelMesh.position.x
+      );
+      player.modelMesh.position.x += Math.cos(angle) * 0.05;
+      player.modelMesh.position.z += Math.sin(angle) * 0.05;
 
-		if (player.moving) {
-			// 걸어가는 상태
-			angle = Math.atan2(
-				destinationPoint.z - player.modelMesh.position.z,
-				destinationPoint.x - player.modelMesh.position.x
-			);
-			player.modelMesh.position.x += Math.cos(angle) * 0.05;
-			player.modelMesh.position.z += Math.sin(angle) * 0.05;
+      camera.position.x = cameraPosition.x + player.modelMesh.position.x;
+      camera.position.z = cameraPosition.z + player.modelMesh.position.z;
 
-			camera.position.x = cameraPosition.x + player.modelMesh.position.x;
-			camera.position.z = cameraPosition.z + player.modelMesh.position.z;
-			
-			player.actions[0].stop();
-			player.actions[1].play();
-			
-			if (
-				Math.abs(destinationPoint.x - player.modelMesh.position.x) < 0.03 &&
-				Math.abs(destinationPoint.z - player.modelMesh.position.z) < 0.03
-			) {
-				player.moving = false;
-				console.log('멈춤');
-			}
+      player.actions[0].stop();
+      player.actions[1].play();
 
-			if (
-				Math.abs(spotMesh.position.x - player.modelMesh.position.x) < 1.5 &&
-				Math.abs(spotMesh.position.z - player.modelMesh.position.z) < 1.5
-			) {
-				if (!house.visible) {
-					console.log('나와');
-					house.visible = true;
-					spotMesh.material.color.set('seagreen');
-					gsap.to(
-						house.modelMesh.position,
-						{
-							duration: 1,
-							y: 1,
-							ease: 'Bounce.easeOut'
-						}
-					);
-					gsap.to(
-						camera.position,
-						{
-							duration: 1,
-							y: 3
-						}
-					);
-				}
-			} else if (house.visible) {
-				console.log('들어가');
-				house.visible = false;
-				spotMesh.material.color.set('yellow');
-				gsap.to(
-					house.modelMesh.position,
-					{
-						duration: 0.5,
-						y: -1.3
-					}
-				);
-				gsap.to(
-					camera.position,
-					{
-						duration: 1,
-						y: 5
-					}
-				);
-			}
-		} else {
-			// 서 있는 상태
-			player.actions[1].stop();
-			player.actions[0].play();
-		}
-	}
+      if (
+        Math.abs(destinationPoint.x - player.modelMesh.position.x) < 0.03 &&
+        Math.abs(destinationPoint.z - player.modelMesh.position.z) < 0.03
+      ) {
+        player.moving = false;
+        console.log("멈춤");
+      }
 
-	renderer.render(scene, camera);
-	window.requestAnimationFrame(draw);
+      if (
+        Math.abs(spotMesh.position.x - player.modelMesh.position.x) < 1.5 &&
+        Math.abs(spotMesh.position.z - player.modelMesh.position.z) < 1.5
+      ) {
+        if (!house.visible) {
+          console.log("나와");
+          house.visible = true;
+          spotMesh.material.color.set("seagreen");
+          gsap.to(house.modelMesh.position, {
+            duration: 1,
+            y: 1,
+            ease: "Bounce.easeOut"
+          });
+          gsap.to(camera.position, {
+            duration: 1,
+            y: 3
+          });
+        }
+      } else if (house.visible) {
+        console.log("들어가");
+        house.visible = false;
+        spotMesh.material.color.set("yellow");
+        gsap.to(house.modelMesh.position, {
+          duration: 0.5,
+          y: -1.3
+        });
+        gsap.to(camera.position, {
+          duration: 1,
+          y: 5
+        });
+      }
+    } else {
+      // 서 있는 상태
+      player.actions[1].stop();
+      player.actions[0].play();
+    }
+  }
+
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(draw);
 }
 
 function checkIntersects() {
-	// raycaster.setFromCamera(mouse, camera);
+  // raycaster.setFromCamera(mouse, camera); // draw 함수에서 실행 중임
 
-	const intersects = raycaster.intersectObjects(meshes);
-	for (const item of intersects) {
-		if (item.object.name === 'floor') {
-			destinationPoint.x = item.point.x;
-			destinationPoint.y = 0.3;
-			destinationPoint.z = item.point.z;
-			player.modelMesh.lookAt(destinationPoint);
+  const intersects = raycaster.intersectObjects(meshes);
+  for (const item of intersects) {
+    if (item.object.name === "floor") {
+      destinationPoint.x = item.point.x;
+      destinationPoint.y = 0.3;
+      destinationPoint.z = item.point.z;
+      player.modelMesh.lookAt(destinationPoint);
 
-			// console.log(item.point)
+      // console.log(item.point)
 
-			player.moving = true;
+      player.moving = true;
 
-			pointerMesh.position.x = destinationPoint.x;
-			pointerMesh.position.z = destinationPoint.z;
-		}
-		break;
-	}
+      pointerMesh.position.x = destinationPoint.x;
+      pointerMesh.position.z = destinationPoint.z;
+    }
+    break;
+  }
 }
 
 function setSize() {
-	camera.left = -(window.innerWidth / window.innerHeight);
-	camera.right = window.innerWidth / window.innerHeight;
-	camera.top = 1;
-	camera.bottom = -1;
+  camera.left = -(window.innerWidth / window.innerHeight);
+  camera.right = window.innerWidth / window.innerHeight;
+  camera.top = 1;
+  camera.bottom = -1;
 
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.render(scene, camera);
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
 }
 
 // 이벤트
-window.addEventListener('resize', setSize);
+window.addEventListener("resize", setSize);
 
 // 마우스 좌표를 three.js에 맞게 변환
 function calculateMousePosition(e) {
-	mouse.x = e.clientX / canvas.clientWidth * 2 - 1;
-	mouse.y = -(e.clientY / canvas.clientHeight * 2 - 1);
+  mouse.x = (e.clientX / canvas.clientWidth) * 2 - 1;
+  mouse.y = -((e.clientY / canvas.clientHeight) * 2 - 1);
 }
 
 // 변환된 마우스 좌표를 이용해 래이캐스팅
 function raycasting() {
-	raycaster.setFromCamera(mouse, camera);
-	checkIntersects();
+  raycaster.setFromCamera(mouse, camera);
+  checkIntersects();
 }
 
 // 마우스 이벤트
-canvas.addEventListener('mousedown', e => {
-	isPressed = true;
-	calculateMousePosition(e);
+canvas.addEventListener("mousedown", (e) => {
+  isPressed = true;
+  calculateMousePosition(e);
 });
-canvas.addEventListener('mouseup', () => {
-	isPressed = false;
+canvas.addEventListener("mouseup", () => {
+  isPressed = false;
 });
-canvas.addEventListener('mousemove', e => {
-	if (isPressed) {
-		calculateMousePosition(e);
-	}
+canvas.addEventListener("mousemove", (e) => {
+  // 마우스 클릭 + 드래그 상태
+  if (isPressed) {
+    calculateMousePosition(e);
+  }
 });
 
 // 터치 이벤트
-canvas.addEventListener('touchstart', e => {
-	isPressed = true;
-	calculateMousePosition(e.touches[0]);
+canvas.addEventListener("touchstart", (e) => {
+  isPressed = true;
+  calculateMousePosition(e.touches[0]);
 });
-canvas.addEventListener('touchend', () => {
-	isPressed = false;
+canvas.addEventListener("touchend", () => {
+  isPressed = false;
 });
-canvas.addEventListener('touchmove', e => {
-	if (isPressed) {
-		calculateMousePosition(e.touches[0]);
-	}
+canvas.addEventListener("touchmove", (e) => {
+  if (isPressed) {
+    calculateMousePosition(e.touches[0]);
+  }
 });
 
 draw();
